038a7d7fc596070f07b1bef2c51a60c94cf1a709
ibrahim <ibrahimsaad6677@gmail.com>
2023-10-14 18:52:13 +0000

    ibrahim

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+/* _printf.c */
+int _printf(const char *format, ...);
+
+#endif
