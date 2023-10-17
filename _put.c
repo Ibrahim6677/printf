305a1e296fef1e6c8a9bcb080ac4f14d5954c529
Nessmakandil <nessmakandil137@gmail.com>
2023-10-17 08:41:12 +0300

    final

--- _put.c
+++ _put.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
 * _puts - print a string with newline
@@ -12,4 +13,26 @@ int _puts(char *str)
 	while (*str)
 		_putchar(*str++);
 	return (str - a);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+			write(1, buf, i);
+			i = 0;
+	}
+	if (c != BUF_FLUSH)
+			buf[i++] = c;
+	return (1);
 }
